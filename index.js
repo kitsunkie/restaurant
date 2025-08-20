119ea80dfa9d1ba8a6a7e3dd1caa0722bddfd2af
kitunkie <nicole.tomanek02@gmail.com>
2025-08-20 08:29:45 +0200

    Das neuste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 app.use(express.static('restaurant/dist/restaurant'));
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
